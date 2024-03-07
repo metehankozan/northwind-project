194c6c81fbf029aa3ac39bf96c714b08ccb6bc1c
metehankozan <metehankozan@hotmail.com.tr>
2024-03-07 21:14:04 +0300

    add store configuration to the app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css'
+import { Provider } from 'react-redux';
+import { configureStore } from './store/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore();
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
